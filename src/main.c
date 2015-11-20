5acda36be7e9de899deaeae5699653aa22320df5
Gareth Pulham <garethpulham@gmail.com>
2015-11-20 01:45:04 +0000

    Improve the output code

--- src/main.c
+++ src/main.c
@@ -26,11 +26,15 @@ int main(int argc, char* argv[]){
 
     printf("Saving routes to csv... ");
     FILE* solution = fopen(argv[argc-1], "w");
+    if (solution == NULL){
+        printf("\x1b[31mFAILED!\x1b[0m\n");
+        return 2;
+    }
     for (size_t i = 0; routes[i]; i++){
         for (customer_t* current = routes[i]; current; current = current->next){
             fprintf(solution, "%f,%f,%d,", (float)current->x, (float)current->y, current->load);
         }
-        fseek(solution, -1, SEEK_CUR);
+        fseek(solution, -1, SEEK_CUR);  // Go back and overwrite the trailing ',' from the last customer
         fprintf(solution, "\n");
     }
     printf("\x1b[32mdone.\x1b[0m\n");
