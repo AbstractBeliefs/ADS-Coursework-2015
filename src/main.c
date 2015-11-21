1408cd323f8257e50d121de549c90980aa65ce91
Gareth Pulham <garethpulham@gmail.com>
2015-11-21 20:33:29 +0000

    Add precision to CPU time report

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]){
     customer_cwt* *routes = solveClarkeWright(depot, customers);  // The important bit
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
     double cputime = (1000.0*end.tv_sec + 1e-6*end.tv_nsec) - (1000.0*start.tv_sec + 1e-6*start.tv_nsec);
-    printf("\x1b[32mdone - %.2f ms CPU time consumed.\x1b[0m\n", cputime);
+    printf("\x1b[32mdone - %.4f ms CPU time consumed.\x1b[0m\n", cputime);
 
     printf("Saving routes to csv... ");
     FILE* solution = fopen(argv[argc-1], "w");
